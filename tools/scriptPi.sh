b9dbf101cf82a41a429a6c935f636804194e1fbe
gpila <g.pila@studenti.unisa.it>
2017-09-17 19:27:41 +0200

    commit 8

--- tools/scriptPi.sh
+++ tools/scriptPi.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update
 sudo apt-get -y install zsh git vim
 git clone https://github.com/gpila/pi3Script
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/gpila/pi3Script/master/tools/installZsh.sh -O -)"
 cd pi3Script/
 cp ./.vimrc ~/
 cp ./.zsh_history ~/
