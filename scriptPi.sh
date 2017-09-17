052911f6224c95a4fa71693f749e9cd2bc87bfba
gpila <g.pila@studenti.unisa.it>
2017-09-17 18:58:41 +0200

    commit 4

--- scriptPi.sh
+++ scriptPi.sh
@@ -2,6 +2,9 @@
 sudo apt-get -y update
 sudo apt-get install zsh git vim console-data
 sudo loadkeys it
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/gpila/pi3Script
+cd pi3Script/
 cp ./.zsh_history ~/
 cp ./vimrc ~/
+cd ~
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
