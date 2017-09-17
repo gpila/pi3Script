cbfaebc6428ba94c053600a5f29bab609ea29e20
gpila <g.pila@studenti.unisa.it>
2017-09-17 19:35:26 +0200

    commit 10

--- tools/scriptPi.sh
+++ tools/scriptPi.sh
@@ -5,6 +5,6 @@ git clone https://github.com/gpila/pi3Script
 sh -c "$(wget https://raw.githubusercontent.com/gpila/pi3Script/master/tools/installZsh.sh -O -)"
 cd pi3Script/
 cp ./.vimrc ~/
-cp ./.zsh_history ~/
+cp ./.zshrc ~/
 sudo apt-get -y install console-data
 sudo loadkeys it
